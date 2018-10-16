a714ecae532d93b973b019f90f301ea330942872
Owner <Owner@DESKTOP-L1OQNCM>
2018-10-16 14:18:47 +0100

    Update 1 --> No errors , Need to add navigation to tabbed pages

--- ChristmasProject/ChristmasProject/MainPage.xaml.cs
+++ ChristmasProject/ChristmasProject/MainPage.xaml.cs
@@ -13,5 +13,30 @@ namespace ChristmasProject
         {
             InitializeComponent();
         }
+
+        private void btnChangeLabel_Clicked(object sender, EventArgs e)
+        {
+            if(entryUsername.Text == User)
+            {
+                Identification = true;
+
+                if(Identification == true)
+                {
+                    lblMessage.Text = "Welcome back " + entryUsername.Text ;
+                    
+                }
+            }
+            else
+            {
+                Identification = false;
+                lblMessage.Text = "Incorrect user name or password. You entered " + entryUsername.Text;
+                entryUsername.Text = "";
+                btnChangeLabel.Text = "Login";
+            }
+
+        }
+
+        Boolean Identification;
+        String User = "Blaine";
     }
 }
